37b0f188d116cbc432a72b09622b69183e2b660b
Malte Schierholz <malte.schierholz@gmail.com>
2022-09-19 20:52:31 +0200

    documentation improved, bug removed

--- R/data.R
+++ R/data.R
@@ -1,11 +1,11 @@
 #' Categories of the The International Standard Classification of Occupations - ISCO-08
 #'
-#' Subset of the ISCO categories dataset bundled with the ESCO v1.1.
+#' Categories from the International Standard Classification of Occupations - ISCO-08. ISCO-08 is a hierarchical classification, consisting of 10 (1-digit) major groups, 43 (2-digit) sub-major groups, 130 (3-digit) minor groups, and 436 (4-digit) unit groups, all of them included in this data set.
 #'
 #' Source: https://esco.ec.europa.eu
-#' More information on the ISCO-08: https://isco-ilo.netlify.app/en/isco-08/
-#'
-#' This service uses the ESCO classification of the European Commission.
+#' This service uses the ESCO classification of the European Commission. The descriptions used here are taken from the ESCO classification (v1.1, Occupations pillar) of the European Commission, which is based on ISCO-08.
+#' 
+#' More information on the ISCO-08: https://isco-ilo.netlify.app/en/isco-08/, https://www.ilo.org/public/english/bureau/stat/isco/isco08/
 #'
 #' @format A data frame with 619 rows and 3 variables:
 #' \describe{
@@ -17,22 +17,48 @@
 
 #' Pretrained ML models to be used with the package.
 #'
-#' @seealso algo_similarity_based_reasoning
+#' @seealso [algo_similarity_based_reasoning()], [train_similarity_based_reasoning()], https://github.com/malsch/occupationCoding
 #' @format A nested list with pretrained machine learning models:
 #' \describe{
 #'   \item{\code{similarity_based_reasoning}}{list. Contains pretrained models to be used with [algo_similarity_based_reasoning()].}
 #'   \item{\code{similarity_based_reasoning$wordwise}}{list. Contains the pretrained model to be used for providing suggestions using full wordwise matching.}
 #'   \item{\code{similarity_based_reasoning$substring}}{list. Contains the pretrained model to be used for providing suggestions using substring matching.}
 #' }
+#' 
+#' This training data always predicts a 5-digit KldB code, with some exceptions: -0004 stands for 'Not precise enough/uncodable', -0006 stands for 'Multiple Jobs', -0012 stands for 'Blue-collar workers', -0019 stands for 'Volunteer/Social Service', and -0030 stands for 'Student assistant'.
+#' 
+#' @source
+#' Data from the following surveys were pooled:
+#'
+#' Antoni, M., Drasch, K., Kleinert, C., Matthes, B., Ruland, M. and Trahms, A. (2010): Arbeiten und Lernen im Wandel * Teil 1: Überblick über die Studie, FDZ-Methodenreport 05/2010, Forschungsdatenzentrum der Bundesagentur für Arbeit im Institut für Arbeitsmarkt- und Berufsforschung, Nuremberg.
+#'
+#' Rohrbach-Schmidt, D., Hall, A. (2013): BIBB/BAuA Employment Survey 2012, BIBB-FDZ Data and Methodological Reports Nr. 1/2013. Version 4.1, Federal Institute for Vocational Education and Training (Research Data Centre), Bonn.
+#'
+#' Lange, C., Finger, J., Allen, J., Born, S., Hoebel, J., Kuhnert, R., Müters, S., Thelen, J., Schmich, P., Varga, M., von der Lippe, E., Wetzstein, M., Ziese, T. (2017): Implementation of the European Health Interview Survey (EHIS) into the German Health Update (GEDA), Archives of Public Health, 75, 1–14.
+#'
+#' Hoffmann, R., Lange, M., Butschalowsky, H., Houben, R., Schmich, P., Allen, J., Kuhnert, R., Schaffrath Rosario, A., Gößwald, A. (2018): KiGGS Wave 2 Cross-Sectional Study—Participant Acquisition, Response Rates and Representativeness, Journal of Health Monitoring, 3, 78–91. (only wave 2)
+#'
+#' Trappmann, M., Beste, J., Bethmann, A., Müller, G. (2013): The PASS Panel Survey after Six Waves, Journal for Labour Market Research, 46, 275–281. (only wave 10)
+#'
+#' Job titles were taken from the following publication:
+#'
+#' Bundesagentur für Arbeit (2019). Gesamtberufsliste der Bundesagentur für Arbeit. Stand: 03.01.2019. https://download-portal.arbeitsagentur.de/files/.
+#' 
+#' Basically, leaving some data anonymization steps aside, we count for each job title from the Gesamtberufsliste (and some additional titles/texts) how many responses from all surveys are similar to this job title, separately for each coded category. Similarity is calculated in two ways, implying that we obtain two different counts: SubstringSimilarity refers to situations where the job title from the Gesamtberufsliste is a substring of the verbal answer; WordwiseSimilarity refers to situations where a word from the verbal answer is identical to a job title from the Gesamtberufsliste, except that one character from this word is allowed to change (Levenshtein distance). These counts are available as two separate files in the data-raw/training-data/ directory of this package. The algorithm to create these counts is available inside an R-package at https://github.com/malsch/occupationCoding, along with further documentation.
+#' 
+#' [train_similarity_based_reasoning()] is then used to train the ML models. See data-raw/pretrained_models.R for details.
 "pretrained_models"
 
 #' German Auxiliary Classification of Occupations (AuxCO)
 #'
 #' Berufs-Hilfsklassifikation mit Tätigkeitsbeschreibungen.
-#'
+#' 
+#' @seealso 
+#' https://github.com/occupationMeasurement/auxiliary-classification, [load_auxco()]
+#' 
+#' @references
 #' Schierholz, Malte; Brenner, Lorraine; Cohausz, Lea; Damminger, Lisa; Fast, Lisa; Hörig, Ann-Kathrin; Huber, Anna-Lena; Ludwig, Theresa; Petry, Annabell; Tschischka, Laura (2018): Vorstellung einer Hilfsklassifikation mit Tätigkeitsbeschreibungen für Zwecke der Berufskodierung. (IAB-Discussion Paper, 2018), Nürnberg, 45 S. https://www.iab.de/183/section.aspx/Publikation/k180509301
 #'
-#' @seealso algo_similarity_based_reasoning
 #' @format A list with data.tables:
 #' \describe{
 #'   \item{\code{categories}}{data.table. Main list of AuxCO categories including their descriptions etc.}
@@ -135,7 +161,7 @@ load_auxco <- function(dir, add_explanations = TRUE) {
     ]
   }
 
-  return(load_auxco)
+  return(auxco)
 }
 
 load_kldb_raw <- function() {
@@ -190,6 +216,10 @@ load_kldb_raw <- function() {
 #' Clean & Load KldB 2010 dataset.
 #'
 #' Use load_kldb_raw() to load the whole dataset.
+#' 
+#' Source: https://www.klassifikationsserver.de/klassService/index.jsp?variant=kldb2010
+#' 
+#' More information on the KldB 2010: https://statistik.arbeitsagentur.de/DE/Navigation/Grundlagen/Klassifikationen/Klassifikation-der-Berufe/KldB2010-Fassung2020/KldB2010-Fassung2020-Nav.html The KldB 2010 has been revised in 2020. These changes have not been implemented here yet.
 #'
 #' @return A cleaned / slimmed version of the KldB 2010.
 #' @export

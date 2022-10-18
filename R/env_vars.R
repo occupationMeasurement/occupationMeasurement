9ee640271accf7b4f98f4572ff08db52a18261b5
Jan Simson <git@simson.io>
2022-10-18 16:55:12 +0200

    Generalize the parsing of env variables and use it for API container

---
+++ R/env_vars.R
@@ -0,0 +1,94 @@
+# Generate app_settings based on environment variables
+create_app_settings_from_env <- function(verbose = FALSE) {
+  if (verbose) message("Loading settings for occupationMeasurement app")
+
+  # Get a list of possible parameters
+  # (They should all be in upper case)
+  possible_params <- create_app_settings |>
+    args() |>
+    as.list() |>
+    names() |>
+    toupper()
+
+  possible_params <- possible_params[
+    !(
+      # Remove parameters starting with "."
+      startsWith(possible_params, ".") |
+        # Remove empty parameters
+        possible_params == ""
+    )
+  ]
+
+  # Parameters that should be evaluated as R code
+  # This should not really be an attack vector, as if you have access to the
+  # env vars you probably also have acccess to the container itself?
+  eval_params <- c(
+    "handle_data",
+    "get_job_suggestion_params"
+  ) |>
+    toupper()
+
+  app_settings_params_from_env <- parse_env_settings(
+    possible_params = possible_params,
+    eval_params = eval_params,
+    verbose = verbose,
+    reference_function = "occupationMeasurement::create_app_settings"
+  )
+
+  # Actually generate the app_settings taking into account defaults etc.
+  app_settings <- do.call(create_app_settings, app_settings_params_from_env)
+
+  return(app_settings)
+}
+
+parse_env_settings <- function(possible_params, eval_params = c(), verbose = FALSE, reference_function = NULL) {
+  # Iterate over all possible parameters and extract them if present
+  settings_params_from_env <- list()
+  for (param in possible_params) {
+    value <- Sys.getenv(param)
+
+    # Try and convert to a number
+    value_num <- suppressWarnings(as.numeric(value))
+
+    # Set value if it exists
+    if (value != "") {
+      # Do a crude conversion from strings to different types
+      if (param %in% eval_params) {
+        # Evaluate parameter as R-Code
+        value <- eval(value)
+      } else if (value == "TRUE") {
+        value <- TRUE
+      } else if (value == "FALSE") {
+        value <- FALSE
+      } else if (!is.na(value_num)) {
+        value <- value_num
+      }
+
+      settings_params_from_env[[param]] <- value
+    }
+  }
+
+  # Convert parameters back to lowercase
+  names(settings_params_from_env) <- names(settings_params_from_env) |>
+    tolower()
+
+  if (verbose) {
+    paste(
+      "Detected variables:",
+      settings_params_from_env |>
+        print() |>
+        utils::capture.output() |>
+        paste(collapse = "\n"),
+      "List of supported environment variables:",
+      possible_params |>
+        print() |>
+        utils::capture.output() |>
+        paste(collapse = "\n"),
+      if (!is.null(reference_function)) paste0("Check ?", reference_function, " for explanations.") else "",
+      sep = "\n"
+    ) |>
+      message()
+  }
+
+  return(settings_params_from_env)
+}
--- docker/api/api.R
+++ docker/api/api.R
@@ -1,9 +1,19 @@
-# Load our custom api
-pr <- occupationMeasurement::api(
-  start = FALSE,
-  # Read ALLOW_ORIGIN from ENV variable
-  allow_origin = Sys.getenv("ALLOW_ORIGIN")
+api_settings <- occupationMeasurement:::parse_env_settings(
+  possible_params = c(
+    "LOG_TO_CONSOLE",
+    "LOG_TO_FILE",
+    "LOG_FILEPATH",
+    "REQUIRE_IDENTIFIER",
+    "ALLOW_ORIGIN"
+  ),
+  verbose = TRUE
 )
+
+# Don't auto-start the api as we do it manually below
+api_settings$start <- FALSE
+
+# Load our custom api
+pr <- do.call(occupationMeasurement::api, api_settings)
 
 # Adapted from the original docker image
 # https://github.com/rstudio/plumber/blob/main/Dockerfile

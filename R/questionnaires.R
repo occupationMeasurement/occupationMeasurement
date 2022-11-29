b033db381f675ef95be77536a38f6c36312a2d71
Jan Simson <git@simson.io>
2022-11-29 17:26:44 +0100

    Update `questionnaire_demo()`

--- R/questionnaires.R
+++ R/questionnaires.R
@@ -98,12 +98,10 @@ questionnaire_demo <- function(show_feedback_page = TRUE) {
     page_welcome(
       title = "Herzlich Willkommen zum Demo-Modul zur automatischen Berufskodierung!",
       render_after = function(...) {
-        list(
-          br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(),
-          hr(),
-          hr(),
+        shiny::tags$div(
+          class = "demo-text",
           h3("Hinweise zum Demo-Modul"),
-          p(strong("Zum Fragebogen:")),
+          h4("Zum Fragebogen:"),
           p("Der im folgenden dargestellte Fragebogen entspricht dem questionnaire_interviewer_administered, der f\u00fcr interviewergest\u00fctzte Befragungen entwickelt wurde (z.B. Telefonbefragungen). Dabei wird Text in drei Farb-Codes verwendet:"),
           tags$ul(
             tags$li("Schwarzer Text: Muss zwingend vom Interviewer vorgelesen werden."),
@@ -111,13 +109,10 @@ questionnaire_demo <- function(show_feedback_page = TRUE) {
             tags$li("Gr\u00fcner Text: Optional. Darf vorgelesen werden, falls dies zur Unterst\u00fctzung des Befragten hilfreich ist.")
           ),
           p("Weitere Erl\u00e4uterungen stehen zu Demonstrationszwecken unterhalb der Buttons."),
-          br(),
-          p(strong("Wem sollen die folgenden Fragen gestellt werden?")),
+          h4("Wem sollen die folgenden Fragen gestellt werden?"),
           p("Die befragte Person ist erwerbst\u00e4tig. Bei Personen ohne Job (Arbeitslose, Sch\u00fcler, Rentner, Hausm\u00e4nner, ...) machen die folgenden Fragen ggf. weniger Sinn. Alle Nicht-Erwerbst\u00e4tigen sollten also bereits vorher herausgefiltert worden sein (prinzipiell lie\u00dfe sich dies \u00e4ndern, aber daf\u00fcr br\u00e4uchten wir Trainingsdaten)."),
           p("Falls Nebenjobs vorhanden sind, muss bereits klargestellt worden sein, dass wir nur die Haupt-Erwerbst\u00e4tigkeit erfassen wollen (oder nachfolgende Formulierungen m\u00fcssen angepasst werden)."),
-          br(),
-          br(),
-          p(strong("Zur Funktionsweise:")),
+          h4("Zur Funktionsweise:"),
           p("Nach Eingabe eines Textes k\u00f6nnen die folgenden Aktionen erfolgen:"),
           tags$ol(
             tags$li("Die Suche nach m\u00f6glichen Berufskategorien bleibt erfolglos. Der Befragte wird in einere weiteren Frage nach zus\u00e4tzlichen Details seines Berufs gefragt und die Suche startet erneut."),
@@ -128,37 +123,79 @@ questionnaire_demo <- function(show_feedback_page = TRUE) {
         )
       }
     ),
-    page_first_freetext(is_interview = TRUE),
+    page_first_freetext(
+      is_interview = TRUE,
+      render_after = function(...) {
+        shiny::tags$div(
+          class = "demo-text",
+          shiny::tags$p("Es gibt mehrere m\u00f6gliche Abl\u00eufe / Flows durch das Tool, je nach Antwort des Befragten."),
+          shiny::tags$p("Mit der Liste an Beispielantworten unten, k\u00f6nnen Sie die unterschiedlichen Abl\u00eufe testen:"),
+          shiny::tags$ol(
+            shiny::tags$li('Standardbeispiel: "Koch"'),
+            shiny::tags$ul(
+              shiny::tags$li("Verschiedene Folgefragen werden gestellt, wenn die Antworten 1, 2 oder 4 ausgew\u00ehlt werden. Auf die Antwort 2 folgt eine Folgefrage zur Aufsichtst\u00etigkeit (strategische Entscheidungsbefugnis: Ja/Nein?, eine \u00fcberaus typische Folgefrage)"),
+              shiny::tags$li('Wenn die Antwort "etwas anderes" gew\u00ehlt wird, wird um eine Pr\u00ezisierung der T\u00etigkeit gebeten f\u00fcr eine sp\u00etere manuelle Kodierung.'),
+            ),
+            shiny::tags$li('Beispiel: "Elektrotechniker"'),
+            shiny::tags$ul(
+              shiny::tags$li("Wenn Antwort 1 ausgew\u00ehlt wird, werden zwei Folgefragen ben\u00f6tigt (ein seltener Fall). Auf die Antworten 1 und 4 folgen Fragen nach dem Anforderungsniveau (Abgeschlossenes Masterstudium: Ja/Nein?, eine weitere \u00fcberaus typische Folgefrage)."),
+            ),
+            shiny::tags$li('Beispiel: "Deponiearbeiter"'),
+            shiny::tags$ul(
+              shiny::tags$li("Antwort 1 ist ein gutes Beispiel daf\u00fcr, welche Antworten bei einer Folgefrage zum Anforderungsniveau in etwa m\u00f6glich sind."),
+            ),
+            shiny::tags$li('Beispiel: "Schiffslotsin"'),
+            shiny::tags$ul(
+              shiny::tags$li("Antwort 1 ist ein gutes Beispiel daf\u00fcr, welche Antworten bei einer Folgefrage zur Aufsichtst\u00etigkeit m\u00f6glich sind."),
+            ),
+            shiny::tags$li('Beispiel: "Briefmarkensammler" (Algorithmus findet keine Vorschl\u00ege)'),
+            shiny::tags$ul(
+              shiny::tags$li('Antwort auf erste und zweite Freitext-Frage jeweils "Briefmarkensammler"'),
+              shiny::tags$li("Der Algorithmus findet keine Vorschl\u00ege und manuelle Kodierung ist n\u00f6tig."),
+            ),
+            shiny::tags$li('Beispiel: "Backwaren" (Algorithmus findet Vorschl\u00ege erst nach Pr\u00ezisierung)'),
+            shiny::tags$ul(
+              shiny::tags$li('Anwort auf erste Frage: "Backwaren", Antwort auf zweite Frage: "In der B\u00eckerei backen"'),
+              shiny::tags$li('Da hier bereits zwei Freitextfragen gestellt wurden, wird bei der Auswahl von "etwas anderes" keine dritte Freitextfrage gestellt.'),
+            ),
+            shiny::tags$li('Beispiel: "Kr\u00eutersammler" (Algorithmus findet nur einen einzigen Vorschlag)'),
+            shiny::tags$ul(
+              shiny::tags$li("Die Frage ist etwas anders formuliert als sonst.")
+            )
+          )
+        )
+      }
+    ),
     # Use a higher aggregate_score_threshold here, since time is more
     # expensive in telephone surveys and reading options alound much slower
     # than reading them yourself in a web-survey
     page_second_freetext(is_interview = TRUE, aggregate_score_threshold = 0.4),
-    page_select_suggestion(is_interview = TRUE,
+    page_select_suggestion(
+      is_interview = TRUE,
       render_after = function(session, ...) {
-        list(
-          br(),
-          hr(),
-          p("- Auf dieser Seite wurde maschinelles Lernen angewandt um die wahrscheinlichsten Antwortoptionen zu finden,
+        shiny::tags$div(
+          class = "demo-text",
+          shiny::tags$ul(
+            shiny::tags$li("- Auf dieser Seite wurde maschinelles Lernen angewandt um die wahrscheinlichsten Antwortoptionen zu finden,
             damit wir Ihren Job in die Klassifikation der Berufe 2010 (KldB 2010) und in die International Standard Classification of Occupations (ISCO-08) einordnen k\u00f6nnen."),
-          br(),
-          p("- Die f\u00fcnf oben gezeigten Antworten sind die f\u00fcnf wahrscheinlichsten berufsbezogenen Beschreibungen,
+            shiny::tags$li("- Die f\u00fcnf oben gezeigten Antworten sind die f\u00fcnf wahrscheinlichsten berufsbezogenen Beschreibungen,
             von denen der Machine-Learning-Algorithmus vorhersagt, dass sie Ihren Job passend beschreiben
             und bei der Zuweisung von KldB- und ISCO-Codes helfen w\u00fcrden."),
-          br(),
-          p(list("- Die Liste aller berufsbezogenenen Beschreibungen besteht aus \u00fcber 1100 m\u00f6glichen Antworten und wurde vom
+            shiny::tags$li(
+              "Die Liste aller berufsbezogenenen Beschreibungen besteht aus \u00fcber 1100 m\u00f6glichen Antworten und wurde vom
             LMU-Team auf der Grundlage der offiziellen KldB-Klassifikation und unter Nutzung der ISCO-Klassifikation entwickelt.
             Die vollst\u00e4ndige Liste der Antworten (und evtl. weitere Nachfragen) sind",
-          tags$a(href="https://github.com/occupationMeasurement/auxiliary-classification", "in der Berufs-Hilfsklassifikation
-            mit T\u00e4tigkeitsbeschreibungen auf Github", target="_blank"),
-            "ersichtlich. Als Sie Ihre berufliche T\u00e4tigkeit auf der vorherigen Seite eingegeben haben, hat der
+              tags$a(href = "https://github.com/occupationMeasurement/auxiliary-classification", "in der Berufs-Hilfsklassifikation
+            mit T\u00e4tigkeitsbeschreibungen auf Github", target = "_blank"),
+              "ersichtlich. Als Sie Ihre berufliche T\u00e4tigkeit auf der vorherigen Seite eingegeben haben, hat der
             Algorithmus auf der Grundlage Ihrer vorherigen Antwort die Wahrscheinlichkeiten
-            aller 1100+ Jobbeschreibungen aus unserer Liste berechnet und zeigt Ihnen die 5 wahrscheinlichsten an.")),
-          br(),
-          p("- Die assoziierten KldB- und ISCO-Codes sind Codes, die vom LMU-Team jeder Antwortoption zugeordnet wurden, weil sie
+            aller 1100+ Jobbeschreibungen aus unserer Liste berechnet und zeigt Ihnen die 5 wahrscheinlichsten an."
+            ),
+            shiny::tags$li("- Die Standard-KldB- und ISCO-Codes sind Codes, die vom LMU-Team jeder Antwortoption zugeordnet wurden, weil sie
             besonders plausibel sind. Allerdings sind manchmal weitere Nachfragen n\u00f6tig um die korrekte Kategorien genau zu
             erfassen. Wenn Nachfragen gestellt werden, sind die hier abgebildeten KldB-/ISCO-Codes daher nicht final."),
-          br(),
-          p("- Unten sehen Sie die technischen Details dieser Berechnungen:"),
+          ),
+          shiny::tags$p("Unten sehen Sie die technischen Details dieser Berechnungen:"),
           renderTable(
             {
               default_suggestion_codes <- get_suggestion_info(
@@ -174,12 +211,12 @@ questionnaire_demo <- function(show_feedback_page = TRUE) {
                 merge(default_suggestion_codes, sort = FALSE, by = "auxco_id")
 
               tabprobabilities <- data.frame(
-                "Beruf ID" = suggestions$auxco_id,
+                "AuxCo_Id" = suggestions$auxco_id,
                 "Wahrscheinlichkeit" = round(suggestions$score, digits = 2),
                 "Berufsuntergruppe" = suggestions$title,
-                "Taetigkeit" = suggestions$task,
-                "Assoziierter Kldb-Code" = suggestions$default_kldb_id,
-                "Assoziierter ISCO-Code" = suggestions$default_isco_id
+                "T\u00etigkeit" = suggestions$task,
+                "Standard_KldB_Code" = suggestions$default_kldb_id,
+                "Standard_ISCO_Code" = suggestions$default_isco_id
               )
               tabprobabilities
             },
